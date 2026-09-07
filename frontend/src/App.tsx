0b91df7424ae09b20336232f47bd31a39a9d802c
federaciocatalanaavpc-dotcom <federaciocatalanaavpc@gmail.com>
2026-09-07 21:24:23 +0200

    Modernitza totes les pantalles de l'espai voluntari

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,6 +20,7 @@ import CanviarContrasenya from './pages/CanviarContrasenya';
 import RutaProtegida from './components/RutaProtegida';
 import RutaFederacio from './components/RutaFederacio';
 import EstatConnexio from './components/EstatConnexio';
+import VoluntariShell from './components/VoluntariShell';
 
 export default function App() {
   return (
@@ -27,142 +28,23 @@ export default function App() {
       <EstatConnexio />
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route
-          path="/"
-          element={
-            <RutaProtegida>
-              <Dashboard />
-            </RutaProtegida>
-          }
-        />
-        <Route
-          path="/agrupacions"
-          element={
-            <RutaProtegida>
-              <Associacions />
-            </RutaProtegida>
-          }
-        />
-        <Route
-          path="/usuaris"
-          element={
-            <RutaFederacio>
-              <GestioUsuaris />
-            </RutaFederacio>
-          }
-        />
-        <Route
-          path="/inventari"
-          element={
-            <RutaProtegida>
-              <Inventari />
-            </RutaProtegida>
-          }
-        />
-        <Route
-          path="/mapa"
-          element={
-            <RutaProtegida>
-              <Mapa />
-            </RutaProtegida>
-          }
-        />
-        <Route
-          path="/documents"
-          element={
-            <RutaProtegida>
-              <Documents />
-            </RutaProtegida>
-          }
-        />
-        <Route
-          path="/documentacio-propia"
-          element={
-            <RutaProtegida>
-              <DocumentacioPropia />
-            </RutaProtegida>
-          }
-        />
-        <Route
-          path="/formacio"
-          element={
-            <RutaProtegida>
-              <Formacio />
-            </RutaProtegida>
-          }
-        />
-        <Route
-          path="/gestio-avpc"
-          element={
-            <RutaProtegida>
-              <GestioAvpc />
-            </RutaProtegida>
-          }
-        />
-        <Route
-          path="/federacio"
-          element={
-            <RutaProtegida>
-              <Federacio />
-            </RutaProtegida>
-          }
-        />
-        <Route
-          path="/voluntari/serveis"
-          element={
-            <RutaProtegida>
-              <PerfilVoluntari />
-            </RutaProtegida>
-          }
-        />
-        <Route
-          path="/voluntari/roba"
-          element={
-            <RutaProtegida>
-              <RobaVoluntari />
-            </RutaProtegida>
-          }
-        />
-        <Route
-          path="/voluntari/disponibilitat"
-          element={
-            <RutaProtegida>
-              <DisponibilitatVoluntari />
-            </RutaProtegida>
-          }
-        />
-        <Route
-          path="/voluntari/estadistiques"
-          element={
-            <RutaProtegida>
-              <EstadistiquesVoluntari />
-            </RutaProtegida>
-          }
-        />
-        <Route
-          path="/voluntari/alertes"
-          element={
-            <RutaProtegida>
-              <AlertesVoluntari />
-            </RutaProtegida>
-          }
-        />
-        <Route
-          path="/avisos"
-          element={
-            <RutaProtegida>
-              <Avisos />
-            </RutaProtegida>
-          }
-        />
-        <Route
-          path="/canviar-contrasenya"
-          element={
-            <RutaProtegida>
-              <CanviarContrasenya />
-            </RutaProtegida>
-          }
-        />
+        <Route path="/" element={<RutaProtegida><Dashboard /></RutaProtegida>} />
+        <Route path="/agrupacions" element={<RutaProtegida><Associacions /></RutaProtegida>} />
+        <Route path="/usuaris" element={<RutaFederacio><GestioUsuaris /></RutaFederacio>} />
+        <Route path="/inventari" element={<RutaProtegida><Inventari /></RutaProtegida>} />
+        <Route path="/mapa" element={<RutaProtegida><Mapa /></RutaProtegida>} />
+        <Route path="/documents" element={<RutaProtegida><Documents /></RutaProtegida>} />
+        <Route path="/documentacio-propia" element={<RutaProtegida><DocumentacioPropia /></RutaProtegida>} />
+        <Route path="/formacio" element={<RutaProtegida><Formacio /></RutaProtegida>} />
+        <Route path="/gestio-avpc" element={<RutaProtegida><GestioAvpc /></RutaProtegida>} />
+        <Route path="/federacio" element={<RutaProtegida><Federacio /></RutaProtegida>} />
+        <Route path="/voluntari/serveis" element={<RutaProtegida><VoluntariShell><PerfilVoluntari /></VoluntariShell></RutaProtegida>} />
+        <Route path="/voluntari/roba" element={<RutaProtegida><VoluntariShell><RobaVoluntari /></VoluntariShell></RutaProtegida>} />
+        <Route path="/voluntari/disponibilitat" element={<RutaProtegida><VoluntariShell><DisponibilitatVoluntari /></VoluntariShell></RutaProtegida>} />
+        <Route path="/voluntari/estadistiques" element={<RutaProtegida><VoluntariShell><EstadistiquesVoluntari /></VoluntariShell></RutaProtegida>} />
+        <Route path="/voluntari/alertes" element={<RutaProtegida><VoluntariShell><AlertesVoluntari /></VoluntariShell></RutaProtegida>} />
+        <Route path="/avisos" element={<RutaProtegida><Avisos /></RutaProtegida>} />
+        <Route path="/canviar-contrasenya" element={<RutaProtegida><CanviarContrasenya /></RutaProtegida>} />
       </Routes>
     </BrowserRouter>
   );
